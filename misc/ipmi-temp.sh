ca5a264419136ff310f9c2103577e3a96dbd9f1a
Gary Shi <garyshi@gmail.com>
2011-05-17 14:50:33 +0800

    ipmi-temp.sh修改成通过变量定义用户名和密码。

--- misc/ipmi-temp.sh
+++ misc/ipmi-temp.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 IPADDR=$1
+USERNAME=super
+PASSWORD=config
+
 # 15: SYS Temp(ESB2) (Temperature): 33.00 C (NA/70.00): [OK]
 # 16: FBD 1.8 Temp (Temperature): 33.00 C (NA/70.00): [OK]
 # 17: Branch1 Temp (Temperature): 28.00 C (NA/70.00): [OK]
 # 18: Ambient Temp (Temperature): 21.00 C (NA/70.00): [OK]
 i=0
-for t in `/usr/sbin/ipmi-sensors -h $IPADDR -u super -p config -s "15 16 17 18" | sed -e 's/.*: \([0-9.]*\) C.*/\1/'`; do
+for t in `/usr/sbin/ipmi-sensors -h $IPADDR -u $USERNAME -p $PASSWORD -s "15 16 17 18" | sed -e 's/.*: \([0-9.]*\) C.*/\1/'`; do
 	i=$[i+1]
 	case "$i" in
 		1) n=temp_ioctl ;;
